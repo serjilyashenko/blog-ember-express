e685e20ea209601bd1066284f379bd957729f968
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-07-15 11:34:36 +0300

    url dasherize fix

--- front/app/adapters/application.js
+++ front/app/adapters/application.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import ENV from '../config/environment';
 
@@ -5,5 +6,11 @@ export default DS.RESTAdapter.extend({
 
   host: ENV.APP.HOST || null,
   namespace: 'api',
+
+  pathForType(type) {
+    const superType = this._super(type);
+
+    return Ember.String.dasherize(superType);
+  },
 
 });
